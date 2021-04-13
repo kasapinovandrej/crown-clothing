ef864342680a48f4ef9761e3e3507585d041d809
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2021-04-13 15:52:25 +0200

    update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import HomePage from './pages/homepage/Homepage';
 import './App.css';
+
+
+const HatsPage = () => {
+  return <div><h1>Heloo Adi</h1></div>
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/adi" component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./menu-item.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, image, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${image})` }}
+        style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
@@ -16,4 +20,4 @@ const MenuItem = ({ title, image, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
