a10e808f9c102e9bedc808698b0a6fa7c33a25d2
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2021-04-19 13:00:46 +0200

    updating app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -3,6 +3,7 @@ import CustomButton from "../custom-button/CustomButton";
 import "./cart-dropdown.scss";
 import CartItem from "../cart-item/CartItem";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ cartItems }) => {
   console.log(cartItems);
@@ -18,6 +19,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems: cartItems });
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
 
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from 'reselect';
+
+//imput selector koji ne koristi createSelector vec samo vracaju deo state-a
+//output selector koristi imput selectore i createSelector da bi se napravili
+
+const selectCart = state => state.cart
+
+//posto koristim createSelector sada je ovo memoized selector
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+)
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((acc, el) => acc + el.quantity, 0)
+)
