fe666c1f849bb7d28947463fd02a1e407bcffb33
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2021-04-20 15:23:36 +0200

    added additem and removeItem func in checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,9 +2,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
 
     if (existingCartItem) {
-        return cartItems.map(cartItem => 
+        return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
         )
     }
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(el => el.id === cartItemToRemove.id)
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(el => el.id !== cartItemToRemove.id)
+    }
+    return cartItems.map(el => el.id === cartItemToRemove.id ? { ...el, quantity: el.quantity - 1 } : el)
+}
